5222159dee65995e130192f0e322b0a80fe2564d
mattrfk <mattrfk@gmail.com>
2017-10-11 16:26:52 -0700

    echo and clear commands

--- terminal.js
+++ terminal.js
@@ -1,45 +1,94 @@
 // TODO: this doesn't seem like the right way to scope this
 var t;
-var lineleader = 'hello$ '
+var lineleader = 'weee $ '
+
+var commands = {
+
+	echo: function(args) {
+		var v = "something went very wrong";
+		if(typeof(args) === 'string') { 
+			v = args; 
+		}
+		else { 
+			v = args.slice(1, args.length).reduce((s, a) => s + a + ' ', '');
+			v = v.substring(0, v.length-1); // oh noooo
+		}
+		t.value += '\n';
+		t.value += v;
+		t.newline;
+	},
+
+	clear: function(args) {
+		t.value = '';
+	}
+}
 
 window.onload = function() {
 
-	// set stuff up
-	t = document.getElementById('shell')
-	t.value = lineleader
-	t.focus()
+	// setup, no upset
+	t = document.getElementById('shell');
+	t.value = lineleader;
+	t.setAttribute("spellcheck", "false");
+	t.focus();
+
+	// add a new line ready for user input
+	t.newline = function() {
+		t.value += '\n' + lineleader;
+	}
 
 	Object.defineProperty(t, 'currentLine', {
 		get: function() { 
-			lines = t.value.split('\n');
-			return lines[lines.length - 1]; },
+			var lines = t.value.split('\n');
+			var line = lines[lines.length - 1];
+			return line.substring(lineleader.length, line.length); 
+		},
 		set: function(l) {
 			t.value = t.value.substring(0, l.length); // remove old
 			t.value += l; // add new
-		},
+		}
 	});
 
 	this.onkeypress = function(event) {
-		// on ENTER, make the newline with lineleader
+		// ENTER: try to run the command
 		if(event.keyCode == 13) {
-			t.value += '\n' + lineleader;
+			
+			args = t.currentLine.split(' ');
+			f = commands[args[0]];
+
+			if(args.length == 1 && args[0] == "") {
+				// do nothing...
+			}
+			else if(typeof f === 'function'){
+				f(args);	
+			}
+			else {
+				commands.echo(args[0] + ": command not found.");
+			}
+			t.newline();
+			
+			console.log(t.scrollHeight);
+			t.scrollTop = t.scrollHeight;
+
 			return false;
+		}
+		else if (t.selectionEnd < t.value.length - t.currentLine.length) {
+			t.selectionEnd = t.selectionStart = t.textLength;
 		}
 	}
 
 	this.onkeydown = function(event) {
-		console.log("keyCode: " + event.keyCode);
+ 		console.log("keyCode: " + event.keyCode);
 
 		// delete
 		if(event.keyCode == 46) {
-			if(t.selectionEnd < t.textLength - (t.currentLine.length - lineleader.length)) {
+			if(t.selectionEnd < t.textLength - t.currentLine.length) {
 				return false; 
 			}
 		}
 
 		// backspace
 		if(event.keyCode == 8) {
-			if(t.selectionEnd <= t.textLength - (t.currentLine.length - lineleader.length)) {
+			if(t.selectionEnd <= t.textLength - t.currentLine.length) {
 				return false; 
 			}
 		}
@@ -48,8 +97,9 @@ window.onload = function() {
 		if(event.keyCode == 46 || event.keyCode == 8) {
 			if(t.currentLine == lineleader) { return false; }
 			if(t.selectionStart != t.selectionEnd && 
-			t.selectionStart < t.textLength - (t.currentLine.length - lineleader.length)) { return false;}
-
-		}  
+			t.selectionStart < t.textLength - t.currentLine.length) { 
+				return false;
+			}
+		}
 	}
 }
